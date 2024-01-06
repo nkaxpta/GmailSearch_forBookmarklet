a43f0c1ec3bc7c6ba6494cd66dc75b2fef377cec
nkaxpta <nkaxpta@ymail.ne.jp>
2024-01-07 04:39:34 +0900

    Updated URL in the codebase

--- GmailSearch.js
+++ GmailSearch.js
@@ -2,8 +2,6 @@ javascript: (function () {
   const result = window.prompt("Gmailで検索");
   const search = `"${result}"`;
   if (result) {
-    window.open(
-      `https://mail.google.com/mail/u/0/?zx=3vkoe8cud4pk#search/${search}`
-    );
+    window.open(`https://mail.google.com/mail/u/0/#search/${search}`);
   }
 })();
